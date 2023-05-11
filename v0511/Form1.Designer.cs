61a81b023f00e5c384fd32a96105e2d782b1cbe5
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-05-11 16:09:25 +0900

    プロパティ

--- v0511/Form1.Designer.cs
+++ v0511/Form1.Designer.cs
@@ -29,24 +29,45 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(243, 150);
+            button1.BackColor = Color.FromArgb(128, 255, 255);
+            button1.Font = new Font("游明朝 Demibold", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(323, 148);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(200, 100);
             button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button1.Text = "押すなよ";
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Black;
+            button2.Font = new Font("Vineta BT", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.FromArgb(192, 0, 0);
+            button2.Location = new Point(285, 121);
+            button2.Name = "button2";
+            button2.Size = new Size(285, 176);
+            button2.TabIndex = 1;
+            button2.Text = "押したな";
+            button2.UseVisualStyleBackColor = false;
+            button2.Visible = false;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(255, 255, 128);
+            ClientSize = new Size(884, 461);
+            Controls.Add(button2);
             Controls.Add(button1);
+            Font = new Font("Yu Gothic UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -55,5 +76,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
--- v0511/Form1.cs
+++ v0511/Form1.cs
@@ -9,7 +9,17 @@ namespace v0511
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("‚±‚ñ‚É‚¿‚í");
+            //MessageBox.Show("‚±‚ñ‚É‚¿‚í");
+            button1.Text = "‰Ÿ‚³‚ê‚½";
+            button1.Left = 100;
+            button1.Visible = false;
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible=false;
+            button1 .Visible=true;
         }
     }
 }
